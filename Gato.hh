b63aaa59f2949b631e72771a902603ddbd288afa
Javier Alejandro <javieralejandro@ubuntu.(none)>
2012-10-29 12:06:32 -0500

    Correccion

--- Gato.hh
+++ Gato.hh
@@ -16,8 +16,27 @@ protected:
 	double Vy=0;
 	double timet=0;
 	QTimer t.start(13);
+	QGraphicsPixmapItem*  m_pic2;
 public:
-	Gato(int peso, QRect rectangulo, int X, int Y):peso(peso), rectangulo(rectangulo), X(X), Y(Y){}
+	Gato(QGraphicsItem* parent, QGraphicsScene* scene, int peso, QRect rectangulo, int X, int Y)
+		:peso(peso), rectangulo(rectangulo), X(X), Y(Y),
+		QGraphicsItem(parent,scene),
+	    m_dx(1.0), m_dy(1.0),
+	    m_scene(scene),
+	    m_pic(0),
+	    m_pic2(0)
+	{
+		velocidad = 100;
+	    angulo = -0.76;
+	    g = 9.8;
+	    avanzar=false;
+	    std::cout << "Construction of BounceBird\n";
+	    QPixmap pmap("bird.png");
+	    QPixmap pmap2("Resortera.png");
+	    m_pic = m_scene->addPixmap(pmap); 
+	    m_pic2 = m_scene->addPixmap(pmap2);
+	    this->setPos(20,500);
+	}
 	~Gato(){}
 	void setVel(double vel)
 	{
