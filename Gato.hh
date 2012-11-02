e6df271f0c50a9f5f985403835d697f4a24f2027
Javier Alejandro <javieralejandro@ubuntu.(none)>
2012-11-02 00:37:37 -0500

    Poderes

--- Gato.hh
+++ Gato.hh
@@ -10,12 +10,18 @@ protected:
 	int Y;
 	int X0;
 	int Y0;
+	int poder;
+	int countiempo;
 	double velocidad;
 	double angulo;
 	double Vx;
 	double Vy;
+	double xx;
+	double yy;
 	double timet;
 	bool avanzar;
+	QLineF LineOne;
+	QLineF LineTwo;
 	//QTimer t.start(13);
 	QGraphicsPixmapItem*  m_pic2;
 public:
@@ -23,19 +29,28 @@ public:
 		:Objeto(parent, scene, peso),
 		X0(Xx), Y0(Yy)
 	{
+		countiempo=0;
 		timet=0;
 		velocidad = 100;
-	    angulo = -0.76;
+	    angulo = -0.75;
 	    g = 9.8;
 	    avanzar=false;
-	    std::cout << "Construction of BounceBird\n";
-	    QPixmap pmap("gato3.png");
+	    std::cout << "KITTEH GUARZ\n";
+	    QPixmap pmap("gato4.png");
 	    QPixmap pmap2("Resortera.png");
 	    m_pic = m_scene->addPixmap(pmap); 
 	    m_pic2 = m_scene->addPixmap(pmap2);
-	    this->setPos(20,500);
+	    this->setPos(80,500);
+	    poder=0;
 	}
 	~Gato(){}
+	QLineF generateLine(QPointF startPos,QPointF endPos)
+	{
+    	if(startPos.x() <endPos.x())
+        	return QLineF(startPos,endPos);
+    	else
+        	return QLineF(endPos,startPos);
+	}
 	void setVel(double vel)
 	{
 		velocidad=vel;
@@ -43,6 +58,11 @@ public:
 	void setAng(double ang)
 	{
 		angulo=ang;
+	}
+	void setIMG(QPixmap pmap)
+	{
+		m_scene->removeItem(m_pic);
+		m_pic = m_scene->addPixmap(pmap);
 	}
 
 	void paint(QPainter *painter, 
@@ -52,7 +72,10 @@ public:
 	  // painter->drawEllipse(this->boundingRect());
 	  //m_pic->setPos(200,500);
 	  m_pic->setPos(this->pos());
-	  m_pic2->setPos(0, 480);
+	  m_pic2->setPos(60, 480);
+	  //m_scene->removeLine(asdasdasd);
+	  LineOne=generateLine(m_pic2->pos(), m_pic->pos());
+	  //m_scene->addLine(asdasdasd);
 	}
 
 	/*void disparar()
@@ -61,17 +84,40 @@ public:
 		velocidad=vals[0];
 		angulo=vals[1];
 	}*/
-	void advance(int /*phase*/)
+	virtual void advance(int /*phase*/)
 	{
+		//if(avanzar == true && velocidad>15)
 		if(avanzar == true)
 		{
-			timet += 0.05;
 			Vx=velocidad*cos(angulo);
 			Vy=velocidad*sin(angulo);
-			double xx=X0+Vx*timet;
-			double yy=Y0+Vy*timet+0.5*g*pow(timet,2);
+			std::cout<<poder<<std::endl;
+			timet += 0.05;
+			xx=X0+Vx*timet;
+			yy=Y0+Vy*timet+0.5*g*pow(timet,2);
 			this->setPos(xx, yy);
+			if (y() + m_dy + (boundingRect().height() * 0.5) > (m_scene->height())-40)
+			{
+      			velocidad=velocidad/1.3;
+      			X0=xx;
+      			Y0=yy;
+      			timet=0;
+      		}
 		}
+		if (avanzar==true && velocidad<=10)
+		{
+			countiempo++;
+			if (countiempo>=200)
+			{
+				m_scene->removeItem(m_pic);
+				this->~Gato();
+				//this->setPos(1000,770);
+			}
+		}
+	}
+	virtual void power()
+	{
+		std::cout<<"";
 	}
 	/*virtual void comprobar()
 	{
@@ -81,18 +127,32 @@ public:
 			timet+=0.05;
 		}
 	}*/
-	 void mousePressEvent(QGraphicsSceneMouseEvent *event)
-	 {
-	     if (event->button() == Qt::LeftButton) {
-	        avanzar=true;
-	        std::cout << "Ahhhh!\n";
+	void avnz()
+	{
+		avanzar=true;
+	}
+	void mousePressEvent(QGraphicsSceneMouseEvent *event)
+	{
+	    if (event->button() == Qt::LeftButton) {
+	     	/*int xg=event->pos().x();
+	     	int yg=event->pos().y();
+	     	this->setPos(xg, yg);
+	     	X0=event->pos().x();
+	     	Y0=event->pos().y();*/
+	        avnz();
+	        poder++;
 	        //this->advance(100);
 	        //m_pic->setPos(this->pos());
-	     } else {
+	        if (poder==2)
+	        {
+	        	std::cout<<"AHHHHHH!";
+	        	power();
+	        }
+	    } else {
 	         // pass on other buttons to base class
 	        QGraphicsItem::mousePressEvent(event);
-	     }
-	 }
+	    }
+	}
 };
 
 #endif
